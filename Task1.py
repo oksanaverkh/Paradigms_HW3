8f81cd6763a37caf7d5e798fe0df1ec95ed9ffd2
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-09-11 22:08:41 +0300

    Final commit

--- Task1.py
+++ Task1.py
@@ -23,91 +23,100 @@ class empty_table():
         return empty_table
 
 
-def check_coordinates():
-    x, y = int(input("x = ")), int(input("y = "))
-    while not 1 <= x <= 3 or not 1 <= y <= 3:
-        print('Wrong number, try again')
-        x, y = int(input()), int(input())
-    return int(x), int(y)
+class coordinates():
+    def check_coordinates():
+        x, y = int(input("x = ")), int(input("y = "))
+        while not 1 <= x <= 3 or not 1 <= y <= 3:
+            print('Wrong number, try again')
+            x, y = int(input()), int(input())
+        return int(x), int(y)
 
 
-def game():
-    player_number = random.randint(1, 2)
-    x_list, y_list = [], []
-    count = 1
-    game_table = empty_table.get_empty_table()
-    while count <= 9:
-        print(f'Player {player_number} enters coordinates (x, y): ', end='')
-        x, y = check_coordinates()
-        x_list.append(x)
-        y_list.append(y)
-        move = (x, y)
+class cross_zeros():
 
-        if count >= 2:
-            for i in range(len(coord_list)):
-                while coord_list[i] == move:
-                    print('Coordinates are already used, enter again')
-                    x, y = check_coordinates()
-                    move = (x, y)
-        for i in range(0, len(game_table)):
-            for j in range(0, len(game_table[i])):
-                if i == x and j == y:
-                    if player_number == 1:
-                        game_table[i][j] = ' X  '
-                    else:
-                        game_table[i][j] = ' O  '
-                print(game_table[i][j], end=' ')
-            print()
-        coord_list = list(zip(x_list, y_list))
-        count += 1
-        player_number = 3-player_number
+    def check_coordinates():
+        x, y = int(input("x = ")), int(input("y = "))
+        while not 1 <= x <= 3 or not 1 <= y <= 3:
+            print('Wrong number, try again')
+            x, y = int(input()), int(input())
+        return int(x), int(y)
 
-    game_table = game_table[1:4]
-    del game_table[0][0]
-    del game_table[1][0]
-    del game_table[2][0]
-    return game_table
+    def game():
+        player_number = random.randint(1, 2)
+        x_list, y_list = [], []
+        count = 1
+        game_table = empty_table.get_empty_table()
+        while count <= 9:
+            print(
+                f'Player {player_number} enters coordinates (x, y): ', end='')
+            x, y = coordinates.check_coordinates()
+            x_list.append(x)
+            y_list.append(y)
+            move = (x, y)
 
-
-game_table = [[' _ |', ' _ |', ' _ '],
-              [' _ |', ' _ |', ' _ '],
-              [' _ |', ' _ |', ' _ ']]
-
-
-def victory_check(game_table):
-    result = 'Dead heat!'
-    count = 0
-    for i in range(len(game_table)):
-        if game_table[i][0] == game_table[i][1] == game_table[i][2]:
-            if game_table[i][0] == ' X  ':
-                result = 'Player 1 WINS!!!'
-            else:
-                result = 'Player 2 WINS!!!'
+            if count >= 2:
+                for i in range(len(coord_list)):
+                    while coord_list[i] == move:
+                        print('Coordinates are already used, enter again')
+                        x, y = coordinates.check_coordinates()
+                        move = (x, y)
+            for i in range(0, len(game_table)):
+                for j in range(0, len(game_table[i])):
+                    if i == x and j == y:
+                        if player_number == 1:
+                            game_table[i][j] = ' X  '
+                        else:
+                            game_table[i][j] = ' O  '
+                    print(game_table[i][j], end=' ')
+                print()
+            coord_list = list(zip(x_list, y_list))
             count += 1
-    for j in range(len(game_table)):
-        if game_table[0][j] == game_table[1][j] == game_table[2][j]:
-            if game_table[0][j] == ' X  ':
-                result = 'Player 1 WINS!!!'
-            else:
-                result = 'Player 2 WINS!!!'
-            count += 1
-    if game_table[0][0] == game_table[1][1] == game_table[2][2] or game_table[2][0] == game_table[1][1] == game_table[0][2]:
-        if game_table[0][0] == ' X  ':
-            result = 'Player 1 WINS!!!'
-        else:
-            result = 'Player 2 WINS!!!'
-    if count > 1:
+            player_number = 3-player_number
+
+        game_table = game_table[1:4]
+        del game_table[0][0]
+        del game_table[1][0]
+        del game_table[2][0]
+        return game_table
+
+    game_table = [[' _ |', ' _ |', ' _ '],
+                  [' _ |', ' _ |', ' _ '],
+                  [' _ |', ' _ |', ' _ ']]
+
+    def victory_check(game_table):
         result = 'Dead heat!'
+        count = 0
+        for i in range(len(game_table)):
+            if game_table[i][0] == game_table[i][1] == game_table[i][2]:
+                if game_table[i][0] == ' X  ':
+                    result = 'Player 1 WINS!!!'
+                else:
+                    result = 'Player 2 WINS!!!'
+                count += 1
+        for j in range(len(game_table)):
+            if game_table[0][j] == game_table[1][j] == game_table[2][j]:
+                if game_table[0][j] == ' X  ':
+                    result = 'Player 1 WINS!!!'
+                else:
+                    result = 'Player 2 WINS!!!'
+                count += 1
+        if game_table[0][0] == game_table[1][1] == game_table[2][2] or game_table[2][0] == game_table[1][1] == game_table[0][2]:
+            if game_table[0][0] == ' X  ':
+                result = 'Player 1 WINS!!!'
+            else:
+                result = 'Player 2 WINS!!!'
+        if count > 1:
+            result = 'Dead heat!'
 
-    return result
+        return result
 
 
 def main():
     print('Start the game:')
     print()
 
-    table_game = game()
-    print(victory_check(table_game))
+    table_game = cross_zeros.game()
+    print(cross_zeros.victory_check(table_game))
 
 
 if __name__ == "__main__":
